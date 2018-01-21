2a2869980247fec3a6f6eda897ac78d3108effe2
den2dd <dd.krutikov@gmail.com>
2018-01-21 15:48:18 +0300

    Update driver Property. Change url.

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -6,14 +6,13 @@ import java.util.concurrent.TimeUnit;
 
 public class MainClass {
     static WebDriver driver;
-
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\eskim\\Desktop\\SeleniumTests\\SberConverter\\drivers\\chromedriver.exe");
-//        System.setProperty("webdriver.gecko.driver", "C:\\Users\\eskim\\Desktop\\SeleniumTests\\SberConverter\\drivers\\geckodriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\eskim\\Desktop\\Selenium_Tests\\SberConverter\\drivers\\chromedriver.exe");
+//        System.setProperty("webdriver.gecko.driver", "C:\\Users\\eskim\\Desktop\\Selenium_Tests\\SberConverter\\drivers\\geckodriver.exe");
 //        driver = new FirefoxDriver();
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-        driver.get("http://www.sberbank.ru/ru/person");
+        driver.get("http://www.sberbank.ru/ru/quotes/converter");
     }
 }
