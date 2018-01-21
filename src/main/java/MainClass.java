d46ebe1a6509c6aff24bba8be513285f42fbb82b
den2dd <dd.krutikov@gmail.com>
2018-01-21 15:31:26 +0300

    Create project SberConverter. Configure pom.xml, MainClass.java

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainClass {
+    static WebDriver driver;
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\eskim\\Desktop\\SeleniumTests\\SberConverter\\drivers\\chromedriver.exe");
+//        System.setProperty("webdriver.gecko.driver", "C:\\Users\\eskim\\Desktop\\SeleniumTests\\SberConverter\\drivers\\geckodriver.exe");
+//        driver = new FirefoxDriver();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("http://www.sberbank.ru/ru/person");
+    }
+}
